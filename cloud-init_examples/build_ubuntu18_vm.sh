ee9248d0fb563124deb4de954f9af44efb2d2243
e-kuznetsov <evgeny.kuznetsov@outlook.com>
2020-07-31 13:20:30 +0000

    test

--- cloud-init_examples/build_ubuntu18_vm.sh
+++ cloud-init_examples/build_ubuntu18_vm.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
+virsh undefine ubuntu18-vm-test
+virsh destroy ubuntu18-vm-test
+rm -f /var/lib/libvirt/images/ubuntu18-vm-test.qcow2
+rm -f /var/lib/libvirt/images/ubuntu18-vm-test-cloud-init.qcow2
 
-cloud-localds -v --network-config=network_config_static.cfg test1-seed.qcow2 cloud_init.cfg
+#qemu-img create -f qcow2 -o preallocation=metadata /var/lib/libvirt/images/ubuntu18-vm-test.qcow2 40G
+#virt-resize --quiet --expand /dev/sda1 /var/lib/libvirt/images/bionic-server-cloudimg-amd64.img /var/lib/libvirt/images/ubuntu18-vm-test.qcow2
+qemu-img create -b /var/lib/libvirt/images/bionic-server-cloudimg-amd64.img \
+                -f qcow2 /var/lib/libvirt/images/ubuntu18-vm-test.qcow2 40G
 
-virt-install --name test1 \
+cloud-localds -v --network-config=network_config_static.cfg /var/lib/libvirt/images/ubuntu18-vm-test-cloud-init.qcow2 cloud_init.cfg
+
+virt-install --name ubuntu18-vm-test \
   --virt-type kvm --memory 2048 --vcpus 2 \
   --cpu host \
-  --disk path=snapshot-bionic-server-cloudimg.qcow2,device=disk \
-  --disk path=test1-seed.qcow2,device=cdrom \
-  --graphics vnc \
+  --disk path=/var/lib/libvirt/images/ubuntu18-vm-test-cloud-init.qcow2,device=cdrom \
+  --disk path=/var/lib/libvirt/images/ubuntu18-vm-test.qcow2,device=disk \
+  --graphics spice \
   --os-type Linux --os-variant ubuntu18.04 \
-  --network bridge=br1,model=virtio \
-  --console pty,target_type=serial
+  --network bridge=br0,model=virtio \
+  --console pty,target_type=serial \
+  --noautoconsole
+
